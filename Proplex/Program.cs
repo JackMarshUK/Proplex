e336a673bd0d9fa57e0baaea09459c548afd0d39
Jack Marsh <jack.marsh@mjquinn.co.uk>
2020-05-30 12:03:51 +0100

    Init

---
+++ Proplex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
